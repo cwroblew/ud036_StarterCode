a25d6b232189fa6bafc659badae46721f3b0d0e9
Charlene Wroblewski <cwroblew@gmail.com>
2017-11-10 12:32:09 -0600

    Modification per reviewer for Movie Trailer Website Project

--- media.py
+++ media.py
@@ -1,7 +1,15 @@
 import webbrowser
 
 class Movie():
-    """ This class provides a way to store movie related information"""
+    """ This class provides a way to store movie related information
+
+    Args:
+        movie_title (str): Title of movie
+        movie_storyline (str): Brief description of the story line of the movie
+        poster_image (str): URL of an image to be used for the movie ie poster
+        trailer_youtube (str): URL of a trailer video for the movie
+
+    """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
